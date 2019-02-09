e19789c0b0139dde21a52670bec9b99dc03f6939
Jerome Lachaud <jerome.lachaud@gmail.com>
2019-02-09 10:09:09 +0100

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,57 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState, useEffect } from 'react'
+import ReactDOM from 'react-dom'
+import axios from 'axios'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Api = async page => {
+  try {
+    const response = await axios({
+      method: 'get',
+      url: `https://randomuser.me/api/?page=${page}&results=5`,
+    })
+    return response
+  } catch (error) {
+    console.error(error)
+  }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function Users() {
+  const [page, setPage] = useState(1)
+  const [users, setUsers] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+
+  const fetchUsers = async (page, users) => {
+    setIsLoading(true)
+    const response = await Api(page)
+    setUsers([...users, ...response.data.results])
+    setIsLoading(false)
+  }
+
+  useEffect(() => {
+    fetchUsers(page, users)
+  }, [page])
+
+  const User = ({
+    user: {
+      name: { first },
+      picture: { large },
+    },
+  }) => <img src={large} alt={first} />
+
+  return (
+    <div>
+      {isLoading ? (
+        <div>Loading ...</div>
+      ) : (
+        <div>
+          {users.map((user, key) => (
+            <User key={key} index={key} user={user} />
+          ))}
+        </div>
+      )}
+      <button onClick={() => setUsers([])}>Reset users</button>
+      <button onClick={() => setPage(page + 1)}>Fetch more users</button>
+    </div>
+  )
+}
+
+ReactDOM.render(<Users />, document.getElementById('root'))
